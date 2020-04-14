ce7a16d7d88e4e8320ad338cb40dbcc4affa5a30
Alex <50577816+Litvinov1618@users.noreply.github.com>
2020-04-14 17:13:18 +0300

    add firebase config

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,19 @@
+import app from 'firebase/app'
+
+const config = {
+  apiKey: "AIzaSyB1cfK1IM4b_U24YALhVjErvfJ1pYlaqiY",
+  authDomain: "fir-test-272f5.firebaseapp.com",
+  databaseURL: "https://fir-test-272f5.firebaseio.com",
+  projectId: "fir-test-272f5",
+  storageBucket: "fir-test-272f5.appspot.com",
+  messagingSenderId: "486137637760",
+  appId: "1:486137637760:web:c2b6ae3c9f7dbf4a2aef65"
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config)
+  }
+}
+
+export default Firebase
